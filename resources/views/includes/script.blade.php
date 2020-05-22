ed3bd662d7a4a3fad57b2ec26e2d21cc2a328c1e
fajargema <fgemar72@gmail.com>
2020-05-22 22:58:03 +0700

    memperbaiki datatable

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -12,11 +12,14 @@
 <script src="{{ asset('vendor/chart.js/Chart.min.js') }}"></script>
 <script src="{{ asset('vendor/datatables/jquery.dataTables.min.js') }}"></script>
 <script src="{{ asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+<script>
+    $(document).ready(function() {
+        $('#dataTable').DataTable();
+    });
+</script>
 
 <!-- Page level custom scripts -->
-<script src="{{ asset('js/demo/chart-area-demo.js') }}"></script>
 <script src="{{ asset('js/demo/chart-pie-demo.js') }}"></script>
-<script src="{{ asset('js/demo/datatables-demo.js') }}"></script>
 
 <script src="https://cdn.ckeditor.com/ckeditor5/19.0.0/classic/ckeditor.js"></script>
 <script>
